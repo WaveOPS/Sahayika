a73b2660e85f719143efb4b2dfe557a39141d874
OmMoholkar <omoholkar30@gmail.com>
2025-04-02 14:52:10 +0530

    added navbar files on /farmer

--- farmer/fnav.php
+++ farmer/fnav.php
@@ -1,182 +1,87 @@
-    <nav
-      id="navbar-main"
-      class="
-        navbar navbar-main navbar-expand-lg
-        bg-default
-        navbar-light
-        position-sticky
-        top-0
-        shadow
-        py-0
-      "
-    >
-      <div class="container-fluid">
-        <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
-          <li class="nav-item dropdown">
-            <a href="../index.php" class="navbar-brand mr-lg-5 text-white">
-                               <img src="../assets/img/nav.png" />
-            </a>
-          </li>
-        </ul>
+<nav
+   id="navbar-main"
+   class="
+     navbar navbar-main navbar-expand-lg
+     navbar-light
+     position-sticky
+     top-0
+     shadow
+     py-2
+   "
+   style="background: #000;"
+>
+   <div class="container">
+     <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
+       <li class="nav-item">
+         <a href="../index.php" class="navbar-brand text-white">
+           <img src="assets/img/sahayika.png" alt="Logo" style="filter: brightness(0.8) sepia(1) hue-rotate(90deg) saturate(2); height: 40px;">
+         </a>
+       </li>
+     </ul>
 
-        <button
-          class="navbar-toggler bg-white"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbar_global"
-          aria-controls="navbar_global"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon text-white"></span>
-        </button>
-        <div class="navbar-collapse collapse bg-default" id="navbar_global">
-          <div class="navbar-collapse-header">
-            <div class="row">
-              <div class="col-10 collapse-brand">
-                <a href="../index.html">
-                  <img src="../assets/img/nav.png" />
-                </a>
-              </div>
-              <div class="col-2 collapse-close bg-danger">
-                <button
-                  type="button"
-                  class="navbar-toggler"
-                  data-toggle="collapse"
-                  data-target="#navbar_global"
-                  aria-controls="navbar_global"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span></span>
-                  <span></span>
-                </button>
-              </div>
-            </div>
-          </div>
+     <button
+       class="navbar-toggler bg-white"
+       type="button"
+       data-toggle="collapse"
+       data-target="#navbar_global"
+       aria-controls="navbar_global"
+       aria-expanded="false"
+       aria-label="Toggle navigation"
+     >
+       <span class="navbar-toggler-icon"></span>
+     </button>
 
-          <ul class="navbar-nav align-items-lg-center ml-auto topnav" id="nav">
-		  
-		  	 
-		  
-	 <li class="nav-item" id="prediction">
-			   <div class="dropdown show ">
-		  <a class="nav-link dropdown-toggle text-white " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		                  <span class="text-white nav-link-inner--text"
-                  ><i class="text-white fas fa-magic"></i> Prediction</span
-                >
-		  </a>
+     <div class="collapse navbar-collapse" id="navbar_global">
+       <ul class="navbar-nav align-items-lg-center ml-auto">
+         <li class="nav-item"><a href="../index.php" class="nav-link text-white">Home</a></li>
 
-		   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-		   <a class="dropdown-item" href="fcrop_prediction.php">Crop Prediction​</a>
-		   <a class="dropdown-item" href="fyield_prediction.php">Yield Prediction​</a>
-			<a class="dropdown-item" href="frainfall_prediction.php">Rainfall Prediction​</a>
-		  </div>
-		</div>
-			</li>
-			
-			
-			
-			 <li class="nav-item" id="recommendation">
-			   <div class="dropdown show ">
-		  <a class="nav-link dropdown-toggle text-white " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		                  <span class="text-white nav-link-inner--text"
-                  ><i class="text-white fas fa-gavel"></i> Recommendation</span
-                >
-		  </a>
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle text-white" href="#" id="predictionDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Prediction</a>
+           <div class="dropdown-menu" aria-labelledby="predictionDropdown">
+             <a class="dropdown-item" href="fcrop_prediction.php">Crop Prediction</a>
+             <a class="dropdown-item" href="fyield_prediction.php">Yield Prediction</a>
+             <a class="dropdown-item" href="frainfall_prediction.php">Rainfall Prediction</a>
+           </div>
+         </li>
 
-		   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-			<a class="dropdown-item" href="fcrop_recommendation.php">Crop Recommendation​</a>
-			<a class="dropdown-item" href="ffertilizer_recommendation.php">Fertilizer Recommendation​</a>
-		  </div>
-		</div>
-			</li>
-			
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle text-white" href="#" id="recommendationDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Recommendation</a>
+           <div class="dropdown-menu" aria-labelledby="recommendationDropdown">
+             <a class="dropdown-item" href="fcrop_recommendation.php">Crop Recommendation</a>
+             <a class="dropdown-item" href="ffertilizer_recommendation.php">Fertilizer Recommendation</a>
+           </div>
+         </li>
 
-		  
-			 <li class="nav-item" id="trade">
-			   <div class="dropdown show ">
-		  <a class="nav-link dropdown-toggle text-white " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		                  <span class="text-white nav-link-inner--text"
-                  ><i class="text-white fas fa-shopping-cart"></i> Trade</span
-                >
-		  </a>
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle text-white" href="#" id="tradeDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Trade</a>
+           <div class="dropdown-menu" aria-labelledby="tradeDropdown">
+             <a class="dropdown-item" href="ftradecrops.php">Trade Crops</a>
+             <a class="dropdown-item" href="fstock_crop.php">Crop Stocks</a>
+             <a class="dropdown-item" href="fselling_history.php">Selling History</a>
+           </div>
+         </li>
 
-		   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-			<a class="dropdown-item" href="ftradecrops.php">Trade Crops​</a>
-			<a class="dropdown-item" href="fstock_crop.php">Crop Stocks​</a>
-			<a class="dropdown-item" href="fselling_history.php">Selling History​</a>
-		  </div>
-		</div>
-			</li>
-			
+         <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle text-white" href="#" id="toolsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tools</a>
+           <div class="dropdown-menu" aria-labelledby="toolsDropdown">
+             <a class="dropdown-item" href="fchatgpt.php">Chat Bot</a>
+             <a class="dropdown-item" href="fweather_prediction.php">Weather Forecast</a>
+             <a class="dropdown-item" href="fnewsfeed.php">News Feed</a>
+           </div>
+         </li>
 
-			 
-			
-						 <li class="nav-item " id="tools">
-			   <div class="dropdown show ">
-		  <a class="nav-link dropdown-toggle text-white " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		                  <span class="text-white nav-link-inner--text"
-                  ><i class="text-white fas fa-gear"></i> Tools</span
-                >
-		  </a>
+         <li class="nav-item">
+           <a href="fprofile.php" class="nav-link text-white">Profile (<?php echo $para2; ?>)</a>
+         </li>
+         
+         <li class="nav-item">
+           <a href="flogout.php" class="nav-link text-white">Logout</a>
+         </li>
+       </ul>
+     </div>
+   </div>
+ </nav>
 
-		   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-		   <a class="dropdown-item" href="fchatgpt.php"> <i class="text-dark fad fa-robot"></i>Chat Bot​</a>
-			<a class="dropdown-item" href="fweather_prediction.php"><i class="text-dark fas fa-cloud"></i> Weather Forecast​</a>
-			<a class="dropdown-item" href="fnewsfeed.php"> <i class="text-dark fas fa-newspaper"></i>News Feed​</a>
-		  </div>
-		</div>
-			</li>
-			
-		
-		
-		   <li class="nav-item" id="profile">
-              <a href="fprofile.php" class="nav-link">
-                <span class="text-white nav-link-inner--text font-weight-bold"
-                  ><i class="text-white fas fa-user"></i> <?php echo $para2 ?> </span
-                >
-              </a>
-            </li>
-			
-			
-		  
-		   <li class="nav-item">
-              <a href="flogout.php" class="nav-link">
-                <span class="text-white nav-link-inner--text font-weight-bold"
-                  ><i class="text-danger fas fa-power-off"></i> Logout </span
-                >
-              </a>
-            </li>
-
-
-
-          </ul>
-        </div>
-      </div>
-    </nav>
-	
-	
-
-<style>
-.topnav a {
-  border-bottom: 3px solid transparent;
-}
-
-.topnav a:hover {
-  border-bottom: 3px solid red;
-}
-
-.topnav a.activa {
-  border-bottom: 3px solid red;
-}
-
-</style>	
-	
-  <script>
-$("#nav li a").each(function() {   
-    if (this.href == window.location.href) {
-        $(this).addClass("activaa");
-    }
-});
-  </script>
+<!-- Ensure Bootstrap JS and jQuery are included -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
---
+++ farmer/navbar.php
@@ -0,0 +1,84 @@
+<!-- filepath: c:\xampp\htdocs\Sahayika\navbar.php -->
+<nav
+   id="navbar-main"
+   class="
+     navbar navbar-main navbar-expand-lg
+     navbar-light
+     position-sticky
+     top-0
+     shadow
+     py-2
+   "
+   style="background: #000;" <!-- Black background -->
+ >
+   <div class="container">
+     <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
+       <li class="nav-item">
+         <a href="index.php" class="navbar-brand text-white">
+           <img src="assets/img/sahayika.png" alt="Logo" style="filter: brightness(0.8) sepia(1) hue-rotate(90deg) saturate(2); height: 40px;">
+         </a>
+       </li>
+     </ul>
+
+     <button
+       class="navbar-toggler bg-white"
+       type="button"
+       data-toggle="collapse"
+       data-target="#navbar_global"
+       aria-controls="navbar_global"
+       aria-expanded="false"
+       aria-label="Toggle navigation"
+     >
+       <span class="navbar-toggler-icon"></span>
+     </button>
+
+     <div class="collapse navbar-collapse" id="navbar_global">
+       <ul class="navbar-nav align-items-lg-center ml-auto">
+         <li class="nav-item">
+           <a href="contact.php" class="nav-link text-white">
+             <i class="fas fa-address-card"></i> Contact
+           </a>
+         </li>
+         <li class="nav-item">
+           <div class="dropdown show">
+             <a
+               class="nav-link dropdown-toggle text-white"
+               href="#"
+               role="button"
+               id="dropdownMenuLink"
+               data-toggle="dropdown"
+               aria-haspopup="true"
+               aria-expanded="false"
+             >
+               <i class="fas fa-user-plus"></i> Sign Up
+             </a>
+             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+               <a class="dropdown-item" href="farmer/fregister.php">Farmer</a>
+               <a class="dropdown-item" href="customer/cregister.php">Customer</a>
+             </div>
+           </div>
+         </li>
+         <li class="nav-item">
+           <div class="dropdown show">
+             <a
+               class="nav-link dropdown-toggle text-white"
+               href="#"
+               role="button"
+               id="dropdownMenuLink"
+               data-toggle="dropdown"
+               aria-haspopup="true"
+               aria-expanded="false"
+             >
+               <i class="fas fa-sign-in-alt"></i> Login
+             </a>
+             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+               <a class="dropdown-item" href="farmer/flogin.php">Farmer</a>
+               <a class="dropdown-item" href="customer/clogin.php">Customer</a>
+               <a class="dropdown-item" href="admin/alogin.php">Admin</a>
+             </div>
+           </div>
+         </li>
+       </ul>
+     </div>
+   </div>
+ </nav>
