9c84a74dd9ab2e9a7c6e6f955309d5130b21b128
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-04-19 11:38:31 +0530

    fixed contact form

--- contact.php
+++ contact.php
@@ -129,6 +129,18 @@
         </div>
     </div>
 </div>
+
+<!-- Required JS for Bootstrap Dropdowns -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+<script>
+  $(document).ready(function () {
+    $('.dropdown-toggle').dropdown();
+  });
+</script>
+
 <?php require("footer.php");?>
 
 </body>
